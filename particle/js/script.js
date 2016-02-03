99c56d993d1a7fdb3f621235a20565bda4539799
Nepios <nepios2@gmail.com>
2016-02-03 09:47:24 -0800

    clean up code and add comments

--- particle/js/script.js
+++ particle/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+//initialize variables and canvas parameters
 var play = true;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
@@ -28,8 +28,8 @@ var requestAnimationFrame =
 canvas.width = 800;
 canvas.height= 400;
 
-
-function Particle(){
+// use constructor to create object made up of particles
+function Particle() {
 	this.x = canvas.width / 2;
 	this.y = canvas.height / 2;
 	this.vx = Math.random() * 10 - 7;
@@ -41,6 +41,8 @@ function Particle(){
 	this.maxLife = 1000;
 	this.color = "rgba(" + parseInt(Math.random()*255, 10) + ', ' + parseInt(Math.random()*255, 10) + ', '+ parseInt(Math.random()*255, 10)+ ', 1)';
 }
+
+// add the draw function to Particle prototype which will be used for animation of the particles
 Particle.prototype.draw = function(){
 	this.x += this.vx;
 	this.y += this.vy;
@@ -53,6 +55,8 @@ Particle.prototype.draw = function(){
 	ctx.arc(this.x, this.y, ballRadius, 0, 2 * Math.PI, true);
 	ctx.fill();
 	ctx.closePath();
+
+	// make the particles bounce off the edges of the canvas
 	if(this.x + this.vx > canvas.width-ballRadius || this.x + this.vx < ballRadius) {
         this.vx = -this.vx;
     	}
@@ -62,6 +66,8 @@ Particle.prototype.draw = function(){
 	this.x += this.vx;
     this.y += this.vy;
     bomb(coordinates[0],coordinates[1], "#00BFFF");
+
+    // test for collisions between the bomb and the particles
     if ((Math.sqrt(Math.pow((this.x-coordinates[0]), 2) + (Math.pow((this.y-coordinates[1]), 2)))) <= bombRadius) {
     	this.life = 1000;
     	bomb(this.x, this.y, this.color);
@@ -75,12 +81,14 @@ Particle.prototype.draw = function(){
     	}	
     }
 
+    // stop animation once the bomb reaches maxRadius
  	if (bombRadius >= maxRadius){
  		getWinner();
     	play = false;
        }
 };
 
+// determine the winner
 function getWinner(){
     if ((player % 2 == 0) && (score > score2)){
     	sweetAlert("Player 1 is the winner!");
@@ -89,8 +97,8 @@ function getWinner(){
     }
 
 };
-
-function createInterval(){
+// create the initial particles
+function createParticle(){
 	play = true;
 	for (var i = 0; i < particleNum; i++){
 		new Particle();
@@ -98,10 +106,10 @@ function createInterval(){
 	animate();
 };
 
+// draw the bomb on the screen and increase radius over time
 function bomb(x, y, color){
 	bombCount++
-	
-	if ((bombCount % 200 === 0) && (bombRadius <= maxRadius)){
+		if ((bombCount % 200 === 0) && (bombRadius <= maxRadius)){
 		bombRadius++;	
 	}
 	ctx.beginPath();
@@ -109,11 +117,6 @@ function bomb(x, y, color){
 	ctx.arc(x, y, bombRadius, 0, 2 * Math.PI, true);
 	ctx.fill();
 	ctx.closePath();
-	// ctx.beginPath();
-	// ctx.fillStyle = "#0000FF";
-	// ctx.arc(coordinates[0], coordinates[1], (bombRadius - 2), 0, 2 * Math.PI, true);
-	// ctx.fill();
-	// ctx.closePath();
 	ctx.beginPath();
 	ctx.fillStyle = "black";
 	ctx.arc(x, y, (bombRadius - 2), 0, 2 * Math.PI, true);
@@ -122,6 +125,7 @@ function bomb(x, y, color){
 	return bombRadius;
 };
 
+// animate the particles and the bomb
 function animate(){
 	ctx.fillStyle = 'rgba(0,0,0,0.1)';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -135,14 +139,13 @@ function animate(){
 		clearCanvas();
 	}
 };
-
+// get the position of the mouse to place the bomb on the canvas
 function getPosition(event){
 	x = event.x;
 	y = event.y;
 	var rect = canvas.getBoundingClientRect();
     x = event.x- rect.left;
     y = event.y - rect.top;
-
 	ctx.beginPath();
 	ctx.fillStyle = 'white';
 	ctx.arc(x, y, 6, 0, 2 * Math.PI, true);
@@ -152,6 +155,7 @@ function getPosition(event){
 
 };
 
+// clear the canvas of all particles and bomb
 function clearCanvas(){
 	ctx.beginPath();
 	ctx.fillStyle = 'rgba(0,0,0,1)';
@@ -159,25 +163,31 @@ function clearCanvas(){
 
 };
 
+// add event listener to the canvas for bomb placement
 canvas.addEventListener("click", function(event){
 	coordinates = getPosition(event);
 }, false);
-$('player1').show();
-$('player2').hide();
 
+// add event listener for click of the start button
 $('#start').on('click', function(){
 	if (play == false){
 		sweetAlert("Please press reset and then play to start a new game.");
 	} else{
+
+		// change highScore on page
 		if (highScore === undefined){	
 			$('.highscore').html('').html('<h4>High Score is 0<h4>');
 		} else {
 			$('.highscore').html('').html('<h4>High Score is ' + highScore +'<h4>');
 		}
 	}	
+	
+	// eliminate ability to add more particles by clicking start again
 	if (extraballs === false){
 		extraballs = true;
 		player++;
+		
+		// indicate the player's turn by turning background div red
 		if (player % 2 === 0){
 			$('.score-2').css('background-color', 'red');
 	     	$('.score-1').css('background-color', 'black');
@@ -185,14 +195,16 @@ $('#start').on('click', function(){
 			$('.score-1').css('background-color', 'red');
 	     	$('.score-2').css('background-color', 'black');
 		}
-		createInterval();
+		// create particles
+		createParticle();
 	}
 
 });
 
+// add eventlistener for click of reset button and clear the canvas
 $('#reset').on('click', function(){
-	play = true;
-	extraballs = false;
+	
+	// add new highScore to localstorage
 	if ((score > highScore) || (highScore === undefined)){
 		localStorage.highScore = score;
 		highScore = localStorage.highScore;
@@ -202,21 +214,28 @@ $('#reset').on('click', function(){
 		highScore = localStorage.highScore;
 		$('.highscore').html('').html('<h4>High Score is ' + highScore +'<h4>');
 	}
+
+	// clear the players' scores from the divs
 	if (player % 2 === 0){
 		$('.score-1').html('').html('<h4>Player 1: 0<h4>');
 		$('.score-2').html('').html('<h4>Player 2: 0<h4>');
 		score = 0;
 		score2 = 0;
 	}
+	// reset variables so game play will start again
+	play = true;
+	extraballs = false;
 	bombRadius = 5;
 	coordinates = [];
+	// delete any remaining particles from the canvase
 	for (particle in particles){
 		delete particles[particle];
 	};
+	// clear the bomb from the screen
 	clearCanvas();
-	ctx.closePath();
-	ctx.beginPath();
 });
+
+// add eventlistener for click on the info button
 $('#info').on('click', function(){
 	sweetAlert("How to Play", "Click on start to initiate the particles, then click anywhere in the box to deploy a bomb and try to take out as many particles as possible.");
 });
